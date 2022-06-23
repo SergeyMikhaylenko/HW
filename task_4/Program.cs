5bbf284aaed43c5457c3c33556dfaf19b55ae996
Sergey_Mikhaylenko <Slad95@mail.ru>
2022-06-24 03:51:53 +0400

    task4

---
+++ task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// ЗАДАЧА 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// (Через тернарный оператор)
+
+Console.WriteLine("Введите первое число");
+sbyte a = Convert.ToSByte(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+sbyte b = Convert.ToSByte(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+sbyte c = Convert.ToSByte(Console.ReadLine());
+sbyte max = b > a ? b : a;
+max = max > c ? max : c;
+
+Console.WriteLine("Максимальное число = " +max);
