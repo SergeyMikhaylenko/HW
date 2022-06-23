a958fd1affdfed2ebbc550f93fdcb592a52acc5c
Sergey_Mikhaylenko <Slad95@mail.ru>
2022-06-24 03:54:57 +0400

    task4_if

---
+++ task_4/task_4_if/Program.cs
@@ -0,0 +1,15 @@
+﻿// ЗАДАЧА 4: (Через оператор if)
+
+Console.WriteLine("Введите первое число");
+short a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+short b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+short c = Convert.ToInt32(Console.ReadLine());
+short max = a;
+if (b > a)
+max = b;
+if (c > max)
+max = c;
+Console.WriteLine("Максимальное число="+max);
+
