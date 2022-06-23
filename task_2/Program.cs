440149c1bc96ba13ce1f812ba2dcc4511a923f8b
Sergey_Mikhaylenko <Slad95@mail.ru>
2022-06-24 03:48:23 +0400

    task2

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// ЗАДАЧА 2: Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число");
+byte a = Convert.ToByte(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+byte b = Convert.ToByte(Console.ReadLine());
+if (a > b)
+{
+Console.Write("Максимальное число "+a);
+}
+else
+{
+Console.Write("Максимальное число "+b);
+}
